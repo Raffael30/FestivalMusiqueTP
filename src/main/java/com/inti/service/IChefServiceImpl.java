6121b8121d453dfb3c42c25064676cc54908cdf9
avallee96 <antoine-vallee@live.fr>
2024-01-19 10:33:02 +0100

    classe service au complet

--- src/main/java/com/inti/service/IChefServiceImpl.java
+++ src/main/java/com/inti/service/IChefServiceImpl.java
@@ -1,5 +1,65 @@
 package com.inti.service;
 
-public class IChefServiceImpl {
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inti.model.Chef;
+import com.inti.model.Lieu;
+import com.inti.repository.IChefRepository;
+import com.inti.repository.ILieuRepository;
+
+@Service
+public class IChefServiceImpl implements IChefService{
+	
+	@Autowired
+	IChefRepository iChefRepository;
+
+	@Override
+	public List<Chef> getAllChef() {
+		return iChefRepository.findAll();
+	}
+
+	@Override
+	public Chef saveChef(Chef chef) {
+		if(chef != null) {
+			return iChefRepository.save(chef);
+		} else {
+			return null;
+		}
+		
+	}
+
+
+	@Override
+	public Chef getChef(Long num) {
+		if(Objects.nonNull(num)) {
+			return iChefRepository.getReferenceById(num);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean updateChef(Chef chef) {
+		if(Objects.nonNull(chef)) {
+			iChefRepository.save(chef);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void deleteChef(Long num) {
+		if(Objects.nonNull(num)) {
+			iChefRepository.deleteById(num);
+		}
+	}
+	
+	@Override
+	public long getChefCount() {
+		return iChefRepository.count();
+	}
 
 }
