0c914e663aa1f976afe6867f003cb6874782d972
reychenne <raphael.eychenne@live.fr>
2024-01-19 10:53:32 +0100

    Annotations Column dans Concert

--- src/main/java/com/inti/model/Concert.java
+++ src/main/java/com/inti/model/Concert.java
@@ -3,14 +3,13 @@ package com.inti.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-import lombok.EqualsAndHashCode.Exclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,7 +22,11 @@ public class Concert {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int num;
+	
+	@Column(name = "NOM")
 	private String nom;
+	
+	@Column(name = "DATECONCERT")
 	private LocalDate dateConcert;
 	
 	@OneToMany(mappedBy = "concert")
