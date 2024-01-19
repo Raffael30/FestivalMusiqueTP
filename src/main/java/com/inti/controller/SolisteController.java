898cac260762471a1ebda5a87c13fdd83fda1ef3
Rohann18 <rohann.bella@hotmail.fr>
2024-01-19 14:37:10 +0100

    Revert "resolve conflicts"
    This reverts commit 12f2b91a4f339c1b3d648dde1da5a1ecfe14132d.

--- src/main/java/com/inti/controller/SolisteController.java
+++ src/main/java/com/inti/controller/SolisteController.java
@@ -1,6 +1,7 @@
 package com.inti.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,30 +20,30 @@ import com.inti.service.ISolisteService;
 @Controller
 @RequestMapping("soliste")
 public class SolisteController {
-
+	
 	@Autowired
 	ISolisteService iss;
-
+	
 	@GetMapping("formSoliste")
 	public String getFormSoliste() {
 		return "affichageSoliste";
 	}
-
+	
 	@GetMapping("getAll")
-	public String getAll(Model m) {
-		List<Soliste> solistes = iss.getAllSoliste();
+	public String getAll(Model m){
+		List<Soliste> solistes= iss.getAllSoliste();
 		long solisteCount = iss.getSolisteCount();
-		m.addAttribute("solistes", solistes);
+		m.addAttribute("solistes",solistes);
 		m.addAttribute("solisteCount",solisteCount);
 		return "affichageSoliste";
 	}
-
+	
 	@PostMapping("saveSoliste")
 	public String saveSoliste(@ModelAttribute("soliste") Soliste soliste) {
 		iss.saveSoliste(soliste);
 		return "redirect:/soliste/getAll";
 	}
-
+	
 	@GetMapping("getSoliste/{num}")
 	public String getSoliste(@PathVariable("num") long num, Model m){
 		Soliste soliste = iss.getSoliste(num);
@@ -50,13 +51,12 @@ public class SolisteController {
 		return "affichageSoliste";
 	}
 
-
 	@PutMapping("updateSoliste")
 	public String updateSoliste(@ModelAttribute("soliste") Soliste soliste) {
 		iss.updateSoliste(soliste);
 		return "affichageSoliste";
 	}
-
+	
 	@GetMapping("deleteSoliste")
 	public String deleteSoliste(@RequestParam(value ="numSoliste") long num) {
 		iss.deleteSoliste(num);
